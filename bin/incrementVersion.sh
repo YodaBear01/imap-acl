eb1f99ec05dd889932bff9205ec1c6d6aca7778d
YodaBear01 <80950748+YodaBear01@users.noreply.github.com>
2021-06-01 11:12:29 +0200

    replace install.rdf woth manifest.json

--- bin/incrementVersion.sh
+++ bin/incrementVersion.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-OLD_VERSION=0.2.7
-NEW_VERSION=0.2.8
+OLD_VERSION=0.2.8
+NEW_VERSION=0.3.0
 git mv Imap-ACL-Extension-$OLD_VERSION.xpi Imap-ACL-Extension-$NEW_VERSION.xpi
 sed -i "s/$OLD_VERSION/$NEW_VERSION/" ../src/chrome/content/about.xul
 sed -i "s/$OLD_VERSION/$NEW_VERSION/" ../src/chrome/content/overlay.js
-sed -i "s/$OLD_VERSION/$NEW_VERSION/" ../src/install.rdf
+sed -i 's/"version": "$OLD_VERSION"/"version": "$NEW_VERSION"/' ../src/manifest.json
 sed -i "s/$OLD_VERSION/$NEW_VERSION/" ./build.sh
